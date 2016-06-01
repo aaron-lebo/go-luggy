1cf75ad57468424faae0186a43d2820a9094b46d
Aaron Lebo <aaron.matthew.lebo@gmail.com>
2016-06-01 17:03:28 -0500

    ignored wikipedia.org

--- luggy.go
+++ luggy.go
@@ -18,11 +18,10 @@ func main() {
 		c.Join("#utdlug")
 	})
 	c.AddCallback("PRIVMSG", func(e *irc.Event) {
-		msg := e.Message()
-		if strings.HasPrefix(msg, "[WIKIPEDIA]") {
-			return
-		}
 		for _, link := range xurls.Relaxed.FindAllString(e.Message(), -1) {
+			if strings.Contains(link, "wikipedia.org") {
+				continue
+			}
 			u, err := url.Parse(link)
 			if err != nil {
 				continue
